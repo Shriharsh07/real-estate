2b0ebab40de9e825bdc18754fc7c3d301e7c6308
Shriharsh07 <shriharshdpattar@gmail.com>
2026-05-26 17:46:45 +0530

    Remove old Node.js server

--- server/routes/uploadRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const auth = require("../middleware/auth");
-const upload = require("../middleware/upload");
-const { uploadImages } = require("../controllers/uploadController");
-
-router.post("/", auth, upload.array("images", 5), uploadImages);
-
-module.exports = router;
